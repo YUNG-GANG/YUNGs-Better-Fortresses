5380566d36e15dc41218b5dcd7dbfa81fc440fe7
Nick Young <yungnickyoung@gmail.com>
2022-09-21 23:37:26 -0400

    Add initial pieces including keep and keep stairs. Dep beta build of YAPI w/ ignoreBounds support.

---
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterfortresses/mixin/NoDeltasInStructuresMixin.java
@@ -0,0 +1,45 @@
+package com.yungnickyoung.minecraft.betterfortresses.mixin;
+
+import com.yungnickyoung.minecraft.betterfortresses.BetterFortressesCommon;
+import com.yungnickyoung.minecraft.betterfortresses.mixin.accessor.WorldGenRegionAccessor;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.levelgen.feature.DeltaFeature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.configurations.DeltaFeatureConfiguration;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author TelepathicGrunt
+ * <a href="https://github.com/TelepathicGrunt/RepurposedStructures">https://github.com/TelepathicGrunt/RepurposedStructures</a>
+ */
+@Mixin(DeltaFeature.class)
+public class NoDeltasInStructuresMixin {
+    @Inject(
+            method = "place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z",
+            at = @At(value = "HEAD"),
+            cancellable = true
+    )
+    private void betterfortresses_noDeltasInStructures(FeaturePlaceContext<DeltaFeatureConfiguration> context, CallbackInfoReturnable<Boolean> cir) {
+        if (!(context.level() instanceof WorldGenRegion)) {
+            return;
+        }
+
+        Registry<Structure> configuredStructureFeatureRegistry = context.level().registryAccess().registryOrThrow(Registry.STRUCTURE_REGISTRY);
+        StructureManager structureManager = ((WorldGenRegionAccessor) context.level()).getStructureManager();
+        Structure fortressStructure = configuredStructureFeatureRegistry.get(new ResourceLocation(BetterFortressesCommon.MOD_ID, "fortress"));
+        if (fortressStructure == null) {
+            return;
+        }
+
+        if (structureManager.getStructureAt(context.origin(), fortressStructure).isValid()) {
+            cir.setReturnValue(false);
+        }
+    }
+}
