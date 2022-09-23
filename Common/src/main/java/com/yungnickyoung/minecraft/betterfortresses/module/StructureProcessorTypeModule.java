f2a776cd79f5767e582d8a3de8710af01cacfb48
Nick Young <yungnickyoung@gmail.com>
2022-09-22 23:58:09 -0400

    Basic keep + random staircase to halls. Added PillarProcessor. TODO: adjust stairway length based on y-coord?

--- Common/src/main/java/com/yungnickyoung/minecraft/betterfortresses/module/StructureProcessorTypeModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterfortresses/module/StructureProcessorTypeModule.java
@@ -1,6 +1,7 @@
 package com.yungnickyoung.minecraft.betterfortresses.module;
 
 import com.yungnickyoung.minecraft.betterfortresses.BetterFortressesCommon;
+import com.yungnickyoung.minecraft.betterfortresses.world.processor.PillarProcessor;
 import com.yungnickyoung.minecraft.betterfortresses.world.processor.LegProcessor;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegister;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -9,4 +10,7 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProc
 public class StructureProcessorTypeModule {
     @AutoRegister("leg_processor")
     public static StructureProcessorType<LegProcessor> LEG_PROCESSOR = () -> LegProcessor.CODEC;
+
+    @AutoRegister("pillar_processor")
+    public static StructureProcessorType<PillarProcessor> PILLAR_PROCESSOR = () -> PillarProcessor.CODEC;
 }
