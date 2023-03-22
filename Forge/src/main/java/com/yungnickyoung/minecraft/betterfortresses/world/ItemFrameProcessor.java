2e769f4b998d45f2faf5790b5f20ba0f02e82276
Nick Young <yungnickyoung@gmail.com>
2023-03-22 18:33:01 -0400

    Convert if -> switch statement for Forge item processor to match Fabric's.

--- Forge/src/main/java/com/yungnickyoung/minecraft/betterfortresses/world/ItemFrameProcessor.java
+++ Forge/src/main/java/com/yungnickyoung/minecraft/betterfortresses/world/ItemFrameProcessor.java
@@ -49,71 +49,83 @@ public class ItemFrameProcessor extends StructureProcessor {
 
             // Set the item in the item frame's NBT
             CompoundTag newNBT = globalEntityInfo.nbt.copy();
-            if (item.equals("\"minecraft:stone_sword\"")) { // Weapon pool
-                String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getWeaponItem(random)).toString();
-                if (randomItemString.equals("minecraft:air")) {
-                    return null;
-                }
-                newNBT.getCompound("Item").putString("id", randomItemString);
-            } else if (item.equals("\"minecraft:iron_ingot\"")) { // Loot pool
-                String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getLootItem(random)).toString();
-                if (randomItemString.equals("minecraft:air")) {
-                    return null;
-                }
-                newNBT.getCompound("Item").putString("id", randomItemString);
-            } else if (item.equals("\"minecraft:cobweb\"")) { // Study pool
-                String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getStudyItem(random)).toString();
-                if (randomItemString.equals("minecraft:air")) {
-                    return null;
-                }
-                // Special case: enchanted books
-                if (randomItemString.equals("minecraft:enchanted_book")) {
-                    // Choose enchantment and level
-                    float f = random.nextFloat();
-                    String enchantment;
-                    if (f < 0.2f) enchantment = "minecraft:fire_aspect";
-                    else if (f < 0.4f) enchantment = "minecraft:fire_protection";
-                    else if (f < 0.6f) enchantment = "minecraft:flame";
-                    else if (f < 0.8f) enchantment = "minecraft:smite";
-                    else enchantment = "minecraft:binding_curse";
-                    int lvl;
-
-                    // Flame and Binding Curse can only be level 1
-                    if (enchantment.equals("minecraft:flame") || enchantment.equals("minecraft:binding_curse")) {
-                        lvl = 1;
-                    } else {
-                        lvl = random.nextFloat() < 0.75f ? 1 : 2;
+            switch (item) {
+                case "\"minecraft:stone_sword\"": { // Weapon pool
+                    String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getWeaponItem(random)).toString();
+                    if (randomItemString.equals("minecraft:air")) {
+                        return null;
                     }
+                    newNBT.getCompound("Item").putString("id", randomItemString);
+                    break;
+                }
+                case "\"minecraft:iron_ingot\"": { // Loot pool
+                    String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getLootItem(random)).toString();
+                    if (randomItemString.equals("minecraft:air")) {
+                        return null;
+                    }
+                    newNBT.getCompound("Item").putString("id", randomItemString);
+                    break;
+                }
+                case "\"minecraft:cobweb\"": { // Study pool
+                    String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getStudyItem(random)).toString();
+                    if (randomItemString.equals("minecraft:air")) {
+                        return null;
+                    }
+                    // Special case: enchanted books
+                    if (randomItemString.equals("minecraft:enchanted_book")) {
+                        // Choose enchantment and level
+                        float f = random.nextFloat();
+                        String enchantment;
+                        if (f < 0.2f) enchantment = "minecraft:fire_aspect";
+                        else if (f < 0.4f) enchantment = "minecraft:fire_protection";
+                        else if (f < 0.6f) enchantment = "minecraft:flame";
+                        else if (f < 0.8f) enchantment = "minecraft:smite";
+                        else enchantment = "minecraft:binding_curse";
+                        int lvl;
 
-                    CompoundTag tag = new CompoundTag();
-                    ListTag storedEnchantments = Util.make(new ListTag(), listTag -> listTag.add(
-                            Util.make(new CompoundTag(), compoundTag -> {
-                                compoundTag.putShort("lvl", (short) lvl);
-                                compoundTag.putString("id", enchantment);
-                            })
-                    ));
-                    tag.put("StoredEnchantments", storedEnchantments);
-                    newNBT.getCompound("Item").put("tag", tag);
+                        // Flame and Binding Curse can only be level 1
+                        if (enchantment.equals("minecraft:flame") || enchantment.equals("minecraft:binding_curse")) {
+                            lvl = 1;
+                        } else {
+                            lvl = random.nextFloat() < 0.75f ? 1 : 2;
+                        }
+
+                        CompoundTag tag = new CompoundTag();
+                        ListTag storedEnchantments = Util.make(new ListTag(), listTag -> listTag.add(
+                                Util.make(new CompoundTag(), compoundTag -> {
+                                    compoundTag.putShort("lvl", (short) lvl);
+                                    compoundTag.putString("id", enchantment);
+                                })
+                        ));
+                        tag.put("StoredEnchantments", storedEnchantments);
+                        newNBT.getCompound("Item").put("tag", tag);
+                    }
+                    newNBT.getCompound("Item").putString("id", randomItemString);
+                    break;
                 }
-                newNBT.getCompound("Item").putString("id", randomItemString);
-            } else if (item.equals("\"minecraft:apple\"")) { // Mess Hall pool
-                String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getMessHallItem(random)).toString();
-                if (randomItemString.equals("minecraft:air")) {
-                    return null;
+                case "\"minecraft:apple\"": { // Mess Hall pool
+                    String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getMessHallItem(random)).toString();
+                    if (randomItemString.equals("minecraft:air")) {
+                        return null;
+                    }
+                    newNBT.getCompound("Item").putString("id", randomItemString);
+                    break;
                 }
-                newNBT.getCompound("Item").putString("id", randomItemString);
-            } else if (item.equals("\"minecraft:nether_wart\"")) { // Alchemy ingredients pool
-                String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getAlchemyItem(random)).toString();
-                if (randomItemString.equals("minecraft:air")) {
-                    return null;
+                case "\"minecraft:nether_wart\"": { // Alchemy ingredients pool
+                    String randomItemString = ForgeRegistries.ITEMS.getKey(ItemFrameChances.get().getAlchemyItem(random)).toString();
+                    if (randomItemString.equals("minecraft:air")) {
+                        return null;
+                    }
+                    newNBT.getCompound("Item").putString("id", randomItemString);
+                    break;
                 }
-                newNBT.getCompound("Item").putString("id", randomItemString);
-            } else if (item.equals("\"minecraft:glowstone_dust\"")) { // In alchemy room. 50% chance of blaze powder
-                if (random.nextBoolean()) {
-                    newNBT.getCompound("Item").putString("id", "minecraft:blaze_powder");
-                } else {
-                    return null;
-                }
+                case "\"minecraft:glowstone_dust\"":  // In alchemy room. 50% chance of blaze powder
+                    if (random.nextBoolean()) {
+                        newNBT.getCompound("Item").putString("id", "minecraft:blaze_powder");
+                    } else {
+                        return null;
+                    }
+                    break;
             }
 
             // Required to suppress dumb log spam
