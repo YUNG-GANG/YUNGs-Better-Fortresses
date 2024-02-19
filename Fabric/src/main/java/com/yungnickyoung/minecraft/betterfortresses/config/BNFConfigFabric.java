b9b70ae3a1bddf63c2d8a59e7629805edc2f8c42
Nick Young <yungnickyoung@gmail.com>
2024-02-19 00:05:35 -0500

    Update config file names. Included in v2.4.0.

--- Fabric/src/main/java/com/yungnickyoung/minecraft/betterfortresses/config/BNFConfigFabric.java
+++ Fabric/src/main/java/com/yungnickyoung/minecraft/betterfortresses/config/BNFConfigFabric.java
@@ -4,7 +4,7 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
-@Config(name="betterfortresses-fabric-1_20")
+@Config(name="betterfortresses-fabric-1_20_4")
 public class BNFConfigFabric implements ConfigData {
     @ConfigEntry.Category("Better Nether Fortresses")
     @ConfigEntry.Gui.TransitiveObject
